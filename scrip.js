3dc6b01fb68f7c4bd49f70147acea74bc7d340cb
thom-id <thom27.idn@gmail.com>
2026-07-30 23:29:54 +0700

    Create scrip.js

---
+++ scrip.js
@@ -0,0 +1,9 @@
+{
+    id: 9,
+    name: 'Nama Item',
+    icon: '🎮',  // pake emoji apa aja
+    price: '100 Gems', // atau '1 WL'
+    category: 'limited', // limited / event / popular
+    badge: '⭐ Limited',
+    badgeClass: 'badge-limited'
+}
